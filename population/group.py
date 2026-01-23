6b4007c573ba9f8673868d6f221447b966185490
exodusfizz-dev <exodusfizz1@gmail.com>
2026-01-23 16:13:50 +0000

    Created some first modules for the project.

---
+++ population/group.py
@@ -0,0 +1,15 @@
+class PopulationGroup:
+    def __init__(self, size, healthcare):
+        self.size = size
+        self.healthcare = healthcare  # 0.0–1.0
+
+        self.base_birth_rate = 0.0002
+        self.base_death_rate = 0.00015
+
+    def tick(self):
+        death_rate = self.base_death_rate * (1.0 - self.healthcare)
+
+        births = self.size * self.base_birth_rate
+        deaths = self.size * death_rate
+
+        self.size += births - deaths
