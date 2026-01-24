79abf2b638097010d49216d0dfe27a0a092cc80a
exodusfizz-dev <exodusfizz1@gmail.com>
2026-01-24 08:23:55 +0000

    2 groups, pop summary for city
    Made it so tick(city) gives a total population of the sum of all groups. Added a second group and changed some things to allow it

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from population import PopulationGroup
 
 
 def main():
-    pop = PopulationGroup(size=1000, healthcare=0.7)
-    city = City([pop])
-
+    pop = [PopulationGroup(size=1000, healthcare=0.7), PopulationGroup(size=500, healthcare=0.4)]
+    city = City(pop)
     for week in range(52):
         city.tick()
-        print(f"Week {week + 1}: population = {int(round(pop.size))}") # This only prints the first group. Considering making rounding a helper function for use elsewhere
+        print(f"Week {week + 1}: population = {int(round(city.total_population))}") # This only prints the first group. Considering making rounding a helper function for use elsewhere
 
 
 if __name__ == "__main__":
