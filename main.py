4d93e36af68266e98c1c1e3c358920ba9c329e5f
exodusfizz-dev <exodusfizz1@gmail.com>
2026-01-25 11:10:36 +0000

    Added a stress systen for healthcare and adjusted migration logic accordingly

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from population import PopulationGroup
 
 
 def main():
-    pop = [PopulationGroup(size=1000, healthcare=0.5), PopulationGroup(size=500, healthcare=0.9)]
+    pop = [PopulationGroup(size=1000, healthcare=0.7, healthcare_capacity = 1100), PopulationGroup(size=500, healthcare=0.9, healthcare_capacity = 600), PopulationGroup(size=2000, healthcare=0.4, healthcare_capacity = 1900)]
     city = City(pop)
     for week in range(52):
         city.tick()
