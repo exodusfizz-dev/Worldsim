c9999b367440e6020480ff4751a8f95a3878debe
erictunn <erictunn@gmail.com>
2026-01-29 21:07:06 +0000

    Improved migration by computing attractiveness and increased code readability

--- population/group.py
+++ population/group.py
@@ -8,10 +8,36 @@ class PopulationGroup:
 
         self.base_birth_rate = 0.0002
         self.base_death_rate = 0.00015
+        
+        self.employment_rate = 0.75 + (self.healthcare * 0.15)
+        self.labour_productivity = 1.0
 
-        self.migration_attractiveness = max(0.45 + (self.healthcare * 0.5), min(0.45 + (self.healthcare * 0.5), 1.0))
+        self.migration_attractiveness = min(0.45 + (self.healthcare * 0.3) + (self.employment_rate * 0.2), max(0.45 + (self.healthcare * 0.3) + (self.employment_rate * 0.2), 1.0))
+
+
 
     def tick(self): # Simulate one time step - e.g. one week for now
+        
+        self.update_demographics()
+
+        self.update_healthcare()
+
+        self.update_employment()
+
+
+    def update_healthcare(self):
+
+        if self.size / self.healthcare_capacity <= 1.0:
+            self.healthcare_modifier = 1.05
+        else:
+            self.healthcare_modifier = (self.healthcare_capacity / self.size) ** 1.1
+
+        self.healthcare = self.base_healthcare * self.healthcare_modifier
+
+        self.migration_attractiveness = min(0.45 + (self.healthcare * 0.3) + (self.employment_rate * 0.2), max(0.45 + (self.healthcare * 0.3) + (self.employment_rate * 0.2), 1.0))
+
+    def update_demographics(self):
+        
         death_rate = self.base_death_rate * (1.0 - self.healthcare)
 
         self.births = self.size * self.base_birth_rate
@@ -22,9 +48,6 @@ class PopulationGroup:
         self.last_births = self.births
         self.last_deaths = self.deaths
 
-        if self.size / self.healthcare_capacity <= 1.0:
-            self.healthcare_modifier = 1.05
-        else:
-            self.healthcare_modifier = (self.healthcare_capacity / self.size) ** 1.1
-
-        self.healthcare = self.base_healthcare * self.healthcare_modifier
+    def update_employment(self):
+        self.employment_rate = 0.75 + (self.healthcare * 0.15)
+        
