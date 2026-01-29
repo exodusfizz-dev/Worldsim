67cac8945684c7dcb4c0e21d682e99209e108116
exodusfizz-dev <exodusfizz1@gmail.com>
2026-01-29 21:08:09 +0000

    Added provinces containing cities. Code stability improvements.

--- main.py
+++ main.py
@@ -11,20 +11,23 @@ def main():
         core.tick()
 
         print(f"Week {week+1}: ")
+        for province, provincename in core.provinces:
+            print(f"Province: {provincename}")
+            for city, cityname in province.cities:
+                print(f"{cityname}: \nPopulation = {int(city.total_population)} \nProductivity = {city.productivity:.2f}")
 
-        for city, cityname in core.cities:
-            print(f"{cityname}: \nPopulation = {int(city.total_population)}")
+                for g in city.sum_population_data():
+                    print(
+                        f"Group {g['group']}: "
+                        f"size = {int(g['size'])}, "
+                        f"healthcare = {g['healthcare']:.3f}, "
+                        f"employment_rate = {g['employment_rate']:.3f}, "
+                    )
 
-            for g in city.get_population_data():
-                print(
-                    f"Group {g['group']}: "
-                    f"size = {int(g['size'])}, "
-                    f"healthcare = {g['healthcare']:.3f}"
-                )
-
-        for migration in g['migrations']: # Prints migration data
-            from_group, amount, to_group = migration
-            print(f"{from_group} -> {to_group}, amount: {amount:.3f}")
+                for migration in city.migrations: # Prints migration data
+                    
+                    from_group, amount, to_group = migration
+                    print(f"{from_group} -> {to_group}, amount: {amount:.3f}")
 
 
 if __name__ == "__main__":  # Temporary main function for testing
