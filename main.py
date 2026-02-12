1a89f2a984e3de6c7b1f7462a0d09fc0389ce111
exodusfizz-dev <exodusfizz1@gmail.com>
2026-02-12 17:01:29 +0000

    Moved report to visualisation. Temporary fixes to imports that broke due to creation of module 'model'.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ This module calls core to run the simulation, and prints outputs
 '''
 from config import CONFIG
 from model.core import Core
+from visualisation.report import report
 
 
 MAIN_CFG = CONFIG["main"]
@@ -29,55 +30,6 @@ def main():
         if REPORTER_CFG.get('enabled', True) and week % REPORTER_CFG.get('report_interval', 1) == 0:
 
             report(week, core)
-
-def report(week, core):
-    '''
-    Outputs data for main
-    
-    :param week: week number (int)
-    :param core: core object
-    '''
-    print(f"------\n------\nWeek {week}: ")
-
-    for province in core.provinces:
-
-        print(f"------\nProvince: {province.name}")
-        for city in province.cities:
-
-            print(f"{city.name}: \nPopulation = {int(city.total_population)}"\
-                f"\nProductivity = {city.productivity:.2f}, "\
-                f"Births = {city.birth_total}, "\
-                f"Deaths = {city.death_total}"
-                )
-
-            for g in city.city_data.sum_population_data():
-                print(
-                    f"Group {g['group']}: "
-                    f"size = {int(g['size'])}, "
-                    f"healthcare = {g['healthcare']:.3f}, "
-                    f"employment_rate = {g['employment_rate']:.3f},"
-                    )
-
-            for f in city.city_data.sum_firm_data():
-                print(
-                    f"Ownership: {f['ownership']}, "                    
-                    f"Good: {f['good']}, "
-                    f"Employed = {f['employed']}, "
-                    f"Total productivity = {f['total_productivity']:.0f},"         
-                )
-
-            for good, amount in city.inv.items():
-                print(f"Good: {good}, Kgs: {amount:.2f}")
-
-            if city.last_food_deficit:
-                print(f"Food deficit: {city.last_food_deficit:.2f} Kgs")
-            else:
-                print("No food deficit")
-
-            for migration in city.migrations: # Prints migration data
-
-                from_group, amount, to_group = migration
-                print(f"{from_group} -> {to_group}, amount: {amount:.3f}")
 
 
 if __name__ == "__main__":  # Temporary main function for testing
